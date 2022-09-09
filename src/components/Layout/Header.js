cd30c66d949d589f3d58b70869eb2c68c09f57e2
Simon Ituen <45456612+SimonItuen@users.noreply.github.com>
2022-09-09 21:01:27 +0100

    header reverted

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,14 +8,14 @@ const Header = (props) => {
     return (
         <Fragment>
         <header className={classes.header}>
-            <h1>GREAT MEALS</h1>
+            <h1>REACTMEALS</h1>
             <HeaderCartButton onClick ={props.onShowCart}/>
         </header>
         <div className={classes['main-image']}>
             <img src={mealsImage} alt="A table full of delicious food!"/>
         </div>
         </Fragment>
-    );
+    ); 
 }
 
 export default Header;
