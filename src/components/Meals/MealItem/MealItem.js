be3e56f9790ba68fb100849a7c0492137e8fc290
Simon Ituen <45456612+SimonItuen@users.noreply.github.com>
2022-10-01 14:42:05 +0100

    fetch meals from backend

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -4,7 +4,7 @@ import classes from "./MealItem.module.css";
 import MealItemForm from "./MealItemForm";
 const MealItem = (props) => {
   const cartCtx = useContext(CartContext);
-  const price = `$${props.price.toFixed(2)};`;
+  const price = `$${props.price.toFixed(2)}`;
 
   const addToCartHandler = (amount) => {
     cartCtx.addItem({
